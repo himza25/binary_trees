e41cfda439fcbd138b5fa8f1994395f88351f1b8
himza25 <himza.of.theking95@gmail.com>
2023-11-29 13:50:48 -0800

    Initial commit with binary tree node implementation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,12 +7,13 @@
  * @value: value to put in the new node
  * Return: pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-		binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
 
-		if (new_node == NULL) {
-				return (NULL);
-		}
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
 		new_node->n = value;
 		new_node->parent = parent;
