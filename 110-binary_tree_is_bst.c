0b089fc2e1e2fa753837f239c0290d86f7ea12a4
himza25 <himza.of.theking95@gmail.com>
2023-11-30 08:19:11 -0800

    Add BST check, insert, array to BST, and search functions

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,32 +1,35 @@
 #include "binary_trees.h"
+#include <limits.h>
 
-int check_bst(const binary_tree_t *tree, int min, int max);
+int is_bst_util(const binary_tree_t *tree, int min, int max);
 
 /**
  * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree
  * @tree: Pointer to the root node of the tree to check
  *
- * Return: 1 if tree is a valid BST, 0 otherwise
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	return (check_bst(tree, INT_MIN, INT_MAX));
+	return (is_bst_util(tree, INT_MIN, INT_MAX));
 }
 
 /**
- * check_bst - Helper function to check BST
- * @tree: Pointer to the root node of the tree to check
- * @min: Minimum value
- * @max: Maximum value
+ * is_bst_util - Utility function to check BST validity
+ * @tree: Pointer to the current node
+ * @min: Minimum allowed value
+ * @max: Maximum allowed value
  *
- * Return: 1 if tree is a valid BST, 0 otherwise
+ * Return: 1 if valid BST, 0 otherwise
  */
-int check_bst(const binary_tree_t *tree, int min, int max)
+int is_bst_util(const binary_tree_t *tree, int min, int max)
 {
 	if (!tree)
 		return (1);
-	if (tree->n < min || tree->n > max)
+
+	if (tree->n <= min || tree->n >= max)
 		return (0);
-	return (check_bst(tree->left, min, tree->n - 1) &&
-		check_bst(tree->right, tree->n + 1, max));
+
+	return (is_bst_util(tree->left, min, tree->n) &&
+			is_bst_util(tree->right, tree->n, max));
 }
