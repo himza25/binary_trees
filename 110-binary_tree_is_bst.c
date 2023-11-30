56115d964e9bcdcfe3a377131395f6664e568618
himza25 <himza.of.theking95@gmail.com>
2023-11-30 08:51:38 -0800

    Add BST check, insert, array to BST, and search functions

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,33 +1,35 @@
 #include "binary_trees.h"
-#include <limits.h>
+#include "limits.h"
 
 /**
- * validate_bst - Recursively checks if a binary tree is a valid BST
- * @tree: Pointer to the current node
- * @min: Minimum valid value
- * @max: Maximum valid value
+ * is_bst_helper - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
+ * @lo: The value of the smallest node visited thus far.
+ * @hi: The value of the largest node visited this far.
  *
- * Return: 1 if tree segment is a valid BST, 0 otherwise
+ * Return: If the tree is a valid BST, 1, otherwise, 0.
  */
-int validate_bst(const binary_tree_t *tree, long min, long max)
+int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
-	if (!tree)
-		return (1);
-
-	if ((long)tree->n <= min || (long)tree->n >= max)
-		return (0);
-
-	return (validate_bst(tree->left, min, (long)tree->n - 1) &&
-			validate_bst(tree->right, (long)tree->n + 1, max));
+	if (tree != NULL)
+	{
+		if (tree->n < lo || tree->n > hi)
+			return (0);
+		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, hi));
+	}
+	return (1);
 }
 
 /**
- * binary_tree_is_bst - Determines if a binary tree is a valid BST
- * @tree: Pointer to the root node of the tree
+ * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 1 if the entire tree is a valid BST, 0 otherwise
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	return (validate_bst(tree, LONG_MIN, LONG_MAX));
+	if (tree == NULL)
+		return (0);
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
