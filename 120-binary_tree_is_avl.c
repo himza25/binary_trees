24d3d8add0be822d40035132dcff5ddf2c0fc4cd
himza25 <himza.of.theking95@gmail.com>
2023-11-30 12:01:42 -0800

    task 30

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,104 +1,71 @@
 #include "binary_trees.h"
+#include "limits.h"
 
-int binary_tree_is_bst(const binary_tree_t *tree);
-int bst_height(const binary_tree_t *tree);
-int abs_diff(int a, int b);
-int is_bst_util(const binary_tree_t *tree, int min, int max);
+/**
+ * height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Description: This function measures the height of a binary tree rooted
+ * at the given node.
+ *
+ * Return: If tree is NULL, the function returns 0, else it returns the height.
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + height(tree->left) : 1;
+		r = tree->right ? 1 + height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
+
+/**
+ * is_avl_helper - Checks if a binary tree is a valid AVL tree.
+ * @tree: A pointer to the root node of the tree to check.
+ * @lo: The value of the smallest node visited thus far.
+ * @hi: The value of the largest node visited this far.
+ *
+ * Description: This function checks if the given binary tree is a valid AVL
+ * tree by ensuring that the difference in heights of left and right subtrees
+ * at any node is at most 1 and that all nodes satisfy the AVL tree property.
+ *
+ * Return: If tree is a valid AVL , it returns 1, otherwise, it returns 0.
+ */
+int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
+{
+	size_t lhgt, rhgt, diff;
+
+	if (tree != NULL)
+	{
+		if (tree->n < lo || tree->n > hi)
+			return (0);
+		lhgt = height(tree->left);
+		rhgt = height(tree->right);
+		diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
+		if (diff > 1)
+			return (0);
+		return (is_avl_helper(tree->left, lo, tree->n - 1) &&
+			is_avl_helper(tree->right, tree->n + 1, hi));
+	}
+	return (1);
+}
 
 /**
  * binary_tree_is_avl - Checks if a binary tree is a valid AVL tree.
  * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 1 if tree is a valid AVL tree, 0 otherwise.
+ * Description: This function checks if binary tree is a valid AVL tree
+ * by calling the `is_avl_helper` function with appropriate parameters.
+ *
+ * Return: 1 if tree is a valid AVL tree, and 0 otherwise.
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
-
-	if (!binary_tree_is_bst(tree))
-		return (0);
-
-	left_height = bst_height(tree->left);
-	right_height = bst_height(tree->right);
-
-	if (abs_diff(left_height, right_height) > 1)
-		return (0);
-
-	if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
-		return (0);
-
-	return (1);
-}
-
-/**
- * binary_tree_is_bst - Checks if a binary tree is a BST.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is a BST, 0 otherwise.
- */
-int binary_tree_is_bst(const binary_tree_t *tree)
-{
-	return (is_bst_util(tree, INT_MIN, INT_MAX));
-}
-
-/**
- * is_bst_util - Utility function to check BST property.
- * @tree: A pointer to the root node of the tree to check.
- * @min: Minimum valid value for a node.
- * @max: Maximum valid value for a node.
- *
- * Return: 1 if tree is a BST, 0 otherwise.
- */
-int is_bst_util(const binary_tree_t *tree, int min, int max)
-{
-	if (tree == NULL)
-		return (1);
-
-	if (tree->n < min || tree->n > max)
-		return (0);
-
-	return (is_bst_util(tree->left, min, tree->n - 1) &&
-			is_bst_util(tree->right, tree->n + 1, max));
-
-
-}
-
-/**
- * bst_height - Calculates the height of a BST.
- * @tree: A pointer to the root node of the BST.
- *
- * Return: Height of the BST.
- */
-int bst_height(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = bst_height(tree->left);
-	right_height = bst_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
-}
-
-/**
- * abs_diff - Calculates the absolute difference between two integers.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: Absolute difference between a and b.
- */
-int abs_diff(int a, int b)
-{
-	if (a > b)
-		return (a - b);
-	else
-		return (b - a);
+	return (is_avl_helper(tree, INT_MIN, INT_MAX));
 }
