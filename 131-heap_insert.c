e4add8cfdeb6fca472c5bbe11f5304fcc254918e
himza25 <himza.of.theking95@gmail.com>
2023-11-30 17:29:54 -0800

    task37 and 35

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,26 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
- *
- * Return: size of the tree
- *         0 if tree is NULL
+ * height - Calculates the height of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ * Return: The height of the tree, or -1 if tree is NULL.
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+int height(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	int left = 0, right = 0;
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (tree == NULL)
+		return (-1);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	return (left > right ? left + 1 : right + 1);
 }
 
 /**
- * swap - swaps nodes when child is greater than parent
- *
- * @arg_node: parent node
- * @arg_child: child node
- * Return: no return
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: Pointer to the root node of the tree.
+ * Return: 1 if the tree is perfect, 0 otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree && height(tree->left) == height(tree->right))
+	{
+		if (height(tree->left) == -1)
+			return (1);
+		if (tree->left && !(tree->left->left) && !(tree->left->right) &&
+			tree->right && !(tree->right->left) && !(tree->right->right))
+			return (1);
+		if (tree && tree->left && tree->right)
+			return (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right));
+	}
+	return (0);
+}
+
+/**
+ * swap - Swaps two nodes in a binary heap.
+ * @arg_node: Double pointer to the parent node.
+ * @arg_child: Double pointer to the child node.
  */
 void swap(heap_t **arg_node, heap_t **arg_child)
 {
@@ -30,123 +52,49 @@ void swap(heap_t **arg_node, heap_t **arg_child)
 	node = *arg_node, child = *arg_child;
 	if (child->n > node->n)
 	{
-		if (child->left)
-			child->left->parent = node;
-		if (child->right)
-			child->right->parent = node;
-		if (node->left == child)
-			node_child = node->right, left_right = 0;
-		else
-			node_child = node->left, left_right = 1;
+		child->left ? child->left->parent = node : 0;
+		child->right ? child->right->parent = node : 0;
+		left_right = (node->left == child) ? 0 : 1;
+		node_child = left_right == 0 ? node->right : node->left;
 		node_left = child->left, node_right = child->right;
-		if (left_right == 0)
-		{
-			child->right = node_child;
-			if (node_child)
-				node_child->parent = child;
-			child->left = node;
-		}
-		else
-		{
-			child->left = node_child;
-			if (node_child)
-				node_child->parent = child;
-			child->right = node;
-		}
+		left_right == 0 ? (child->right = node_child, child->left = node)
+						: (child->left = node_child, child->right = node);
+		node_child ? node_child->parent = child : 0;
 		if (node->parent)
-		{
-			if (node->parent->left == node)
-				node->parent->left = child;
-			else
-				node->parent->right = child;
-		}
+			node->parent->left == node ? (node->parent->left = child)
+									   : (node->parent->right = child);
 		parent = node->parent, child->parent = parent;
-		node->parent = child, node->left = node_left;
-		node->right = node_right, *arg_node = child;
+		node->parent = child, node->left = node_left, node->right = node_right;
+		*arg_node = child;
 	}
 }
 
 /**
- * height - measures the height of a tree
- *
- * @tree: tree root
- * Return: height
- */
-int height(const binary_tree_t *tree)
-{
-	int left = 0;
-	int right = 0;
-
-	if (tree == NULL)
-		return (-1);
-
-	left = height(tree->left);
-	right = height(tree->right);
-
-	if (left > right)
-		return (left + 1);
-
-	return (right + 1);
-}
-
-/**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- *
- * @tree: tree root
- * Return: 1 if tree is perfect, 0 otherwise
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	if (tree && height(tree->left) == height(tree->right))
-	{
-		if (height(tree->left) == -1)
-			return (1);
-
-		if ((tree->left && !((tree->left)->left) && !((tree->left)->right))
-		    && (tree->right && !((tree->right)->left) && !((tree->right)->right)))
-			return (1);
-
-		if (tree && tree->left && tree->right)
-			return (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right));
-	}
-
-	return (0);
-}
-
-/**
- * heap_insert - Inserts a value in a Max Binary Heap
- * @root: Double pointer to the root node of the Heap
- * @value: The value to store in the node to be inserted
- * Return: Pointer to the created node, or NULL on failure
+ * heap_insert - Inserts a value into a Max Binary Heap.
+ * @root: Double pointer to the root node of the heap.
+ * @value: The value to insert.
+ * Return: Pointer to the created node, or NULL on failure.
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *tree;
-	int size, leaves, sub, bit, level;
+	heap_t *new_node;
 
-	if (!root)
-		return (NULL);
-
-	if (!(*root))
+	if (*root == NULL)
 	{
 		*root = binary_tree_node(NULL, value);
 		return (*root);
 	}
 
-	tree = *root;
-	size = binary_tree_size(tree);
-	for (level = 0, sub = 1, leaves = size; leaves >= sub; sub *= 2, level++)
-		leaves -= sub;
+	if (binary_tree_is_perfect(*root) || !binary_tree_is_perfect((*root)->left))
+	{
+		new_node = (*root)->left ? heap_insert(&((*root)->left), value)
+								 : ((*root)->left = binary_tree_node(*root, value));
+		swap(root, &((*root)->left));
+		return (new_node);
+	}
 
-	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
-		tree = leaves & bit ? tree->right : tree->left;
-
-	new_node = binary_tree_node(tree, value);
-	leaves & 1 ? (tree->right = new_node) : (tree->left = new_node);
-
-	while (new_node->parent && new_node->n > new_node->parent->n)
-		swap(&(new_node->parent), &new_node);
-
+	new_node = (*root)->right ? heap_insert(&((*root)->right), value)
+							  : ((*root)->right = binary_tree_node(*root, value));
+	swap(root, &((*root)->right));
 	return (new_node);
 }
