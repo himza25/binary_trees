2d83fd0d549cc92e387100d8c25d8fa548787103
himza25 <himza.of.theking95@gmail.com>
2023-11-30 17:21:12 -0800

    task37 and 35

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,25 +1,138 @@
 #include "binary_trees.h"
 
-/* binary_tree_size from the second code */
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
 
-/* swap function from the first code */
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
 
 /**
- * heap_insert - inserts a value in Max Binary Heap
- * @root: a double pointer to the root node of the Heap to insert the value
- * @value: the value to store in the node to be inserted
+ * swap - swaps nodes when child is greater than parent
  *
- * Return: a pointer to the created node, NULL on failure
+ * @arg_node: parent node
+ * @arg_child: child node
+ * Return: no return
+ */
+void swap(heap_t **arg_node, heap_t **arg_child)
+{
+	heap_t *node, *child, *node_child, *node_left, *node_right, *parent;
+	int left_right;
+
+	node = *arg_node, child = *arg_child;
+	if (child->n > node->n)
+	{
+		if (child->left)
+			child->left->parent = node;
+		if (child->right)
+			child->right->parent = node;
+		if (node->left == child)
+			node_child = node->right, left_right = 0;
+		else
+			node_child = node->left, left_right = 1;
+		node_left = child->left, node_right = child->right;
+		if (left_right == 0)
+		{
+			child->right = node_child;
+			if (node_child)
+				node_child->parent = child;
+			child->left = node;
+		}
+		else
+		{
+			child->left = node_child;
+			if (node_child)
+				node_child->parent = child;
+			child->right = node;
+		}
+		if (node->parent)
+		{
+			if (node->parent->left == node)
+				node->parent->left = child;
+			else
+				node->parent->right = child;
+		}
+		parent = node->parent, child->parent = parent;
+		node->parent = child, node->left = node_left;
+		node->right = node_right, *arg_node = child;
+	}
+}
+
+/**
+ * height - measures the height of a tree
+ *
+ * @tree: tree root
+ * Return: height
+ */
+int height(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ *
+ * @tree: tree root
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree && height(tree->left) == height(tree->right))
+	{
+		if (height(tree->left) == -1)
+			return (1);
+
+		if ((tree->left && !((tree->left)->left) && !((tree->left)->right))
+		    && (tree->right && !((tree->right)->left) && !((tree->right)->right)))
+			return (1);
+
+		if (tree && tree->left && tree->right)
+			return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
+
+	return (0);
+}
+
+/**
+ * heap_insert - Inserts a value in a Max Binary Heap
+ * @root: Double pointer to the root node of the Heap
+ * @value: The value to store in the node to be inserted
+ * Return: Pointer to the created node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *tree, *new;
+	heap_t *new_node, *tree;
 	int size, leaves, sub, bit, level;
 
 	if (!root)
 		return (NULL);
+
 	if (!(*root))
-		return (*root = binary_tree_node(NULL, value));
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
 
 	tree = *root;
 	size = binary_tree_size(tree);
@@ -29,16 +142,11 @@ heap_t *heap_insert(heap_t **root, int value)
 	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
 		tree = leaves & bit ? tree->right : tree->left;
 
-	new = binary_tree_node(tree, value);
-	if (leaves & 1)
-		tree->right = new;
-	else
-		tree->left = new;
+	new_node = binary_tree_node(tree, value);
+	leaves & 1 ? (tree->right = new_node) : (tree->left = new_node);
 
-	while (new->parent && new->n > new->parent->n)
-	{
-		swap(&(new->parent), &new);
-	}
+	while (new_node->parent && new_node->n > new_node->parent->n)
+		swap(&(new_node->parent), &new_node);
 
-	return (new);
+	return (new_node);
 }
