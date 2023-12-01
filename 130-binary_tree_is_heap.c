a110fe52160e56d9d59d51bc098475ad2100bfe9
himza25 <himza.of.theking95@gmail.com>
2023-11-30 16:59:01 -0800

    task36

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - calcul the size of a binary tree
+ * binary_tree_size - measures the size of a binary tree
  *
  * @tree: tree root
  * Return: size of the tree or 0 if tree is NULL;
@@ -11,13 +11,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1);
 }
 
 /**
- * tree_is_complete - checks tree
+ * tree_is_complete - checks if tree is complete
  *
- * @tree: pointer tree root
+ * @tree: pointer to the tree root
  * @i: node index
  * @cnodes: number of nodes
  * Return: 1 if tree is complete, 0 otherwise
@@ -35,23 +36,47 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 }
 
 /**
+ * binary_tree_is_complete - calls to tree_is_complete function
+ *
+ * @tree: tree root
+ * Return: 1 if tree is complete, 0 otherwise
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t cnodes;
+
+	if (tree == NULL)
+		return (0);
+
+	cnodes = binary_tree_size(tree);
+
+	return (tree_is_complete(tree, 0, cnodes));
+}
+
+/**
  * check_max_heap - checks if a binary tree node follows max heap property
- * @node: pointer to the current node
+ * @tree: pointer to the current node
  * Return: 1 if node follows max heap property, 0 otherwise
  */
-int check_max_heap(const binary_tree_t *node)
+int check_max_heap(const binary_tree_t *tree)
 {
-	if (node == NULL)
+	if (!tree)
+		return (1);
+	if (tree->left)
+	{
+		if (tree->n < tree->left->n)
+			return (0);
+		if (!check_max_heap(tree->left))
+			return (0);
+	}
+	if (tree->right)
+	{
+		if (tree->n < tree->right->n)
+			return (0);
+		if (!check_max_heap(tree->right))
+			return (0);
+	}
 	return (1);
-
-	if (node->left == NULL && node->right == NULL)
-	return (1);
-
-	if (node->right == NULL)
-	return (node->n >= node->left->n);
-
-	return (node->n >= node->left->n && node->n >= node->right->n &&
-		check_max_heap(node->left) && check_max_heap(node->right));
 }
 
 /**
@@ -61,8 +86,7 @@ int check_max_heap(const binary_tree_t *node)
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	if (tree == NULL || !binary_tree_is_complete(tree))
-	return (0);
-
+	if (!tree || !binary_tree_is_complete(tree))
+		return (0);
 	return (check_max_heap(tree));
 }
