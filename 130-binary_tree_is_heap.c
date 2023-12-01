96d0ddeb134e9a543b2de444ce83f90cb61ec761
himza25 <himza.of.theking95@gmail.com>
2023-11-30 16:35:02 -0800

    task36

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 size_t tree_size(const binary_tree_t *tree);
-int is_tree_complete(const binary_tree_t *tree, int index, int node_count);
-int is_heap_valid(const binary_tree_t *tree);
+int is_complete(const binary_tree_t *tree, int index, int node_count);
+int has_heap_property(const binary_tree_t *tree);
 
 /**
- * binary_tree_is_heap - Checks if a binary tree is a valid Max Binary Heap
+ * binary_tree_is_heap - Checks if a binary tree is a Max Binary Heap
  * @tree: Pointer to the root node of the tree
  *
  * Return: 1 if tree is a valid Max Binary Heap, 0 otherwise
@@ -17,7 +17,7 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 
 	int node_count = tree_size(tree);
 
-	return (is_tree_complete(tree, 0, node_count) && is_heap_valid(tree));
+	return (is_complete(tree, 0, node_count) && has_heap_property(tree));
 }
 
 /**
@@ -35,14 +35,14 @@ size_t tree_size(const binary_tree_t *tree)
 }
 
 /**
- * is_tree_complete - Checks if a binary tree is complete
+ * is_complete - Checks if a binary tree is complete
  * @tree: Pointer to the root node of the tree
  * @index: Index of the current node
  * @node_count: Total number of nodes in the tree
  *
  * Return: 1 if tree is complete, 0 otherwise
  */
-int is_tree_complete(const binary_tree_t *tree, int index, int node_count)
+int is_complete(const binary_tree_t *tree, int index, int node_count)
 {
 	if (!tree)
 		return (1);
@@ -50,17 +50,17 @@ int is_tree_complete(const binary_tree_t *tree, int index, int node_count)
 	if (index >= node_count)
 		return (0);
 
-	return (is_tree_complete(tree->left, 2 * index + 1, node_count) &&
-		is_tree_complete(tree->right, 2 * index + 2, node_count));
+	return (is_complete(tree->left, 2 * index + 1, node_count) &&
+		is_complete(tree->right, 2 * index + 2, node_count));
 }
 
 /**
- * is_heap_valid - Checks if a tree satisfies Max Heap properties
+ * has_heap_property - Checks if a tree satisfies Max Heap properties
  * @tree: Pointer to the root node of the tree
  *
  * Return: 1 if tree satisfies Max Heap properties, 0 otherwise
  */
-int is_heap_valid(const binary_tree_t *tree)
+int has_heap_property(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (1);
@@ -70,5 +70,5 @@ int is_heap_valid(const binary_tree_t *tree)
 	if (tree->right && tree->right->n > tree->n)
 		return (0);
 
-	return (is_heap_valid(tree->left) && is_heap_valid(tree->right));
+	return (has_heap_property(tree->left) && has_heap_property(tree->right));
 }
